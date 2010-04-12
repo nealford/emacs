2061f4d02fa9cd8a507cb408e58f56063a510105
Neal Ford <neal.ford@gmail.com>
2010-04-11 19:23:29 -0600

    updates to snippet infrastructure

--- customizations.el
+++ customizations.el
@@ -4,9 +4,9 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
- '(aquamacs-customization-version-id 172 t)
- '(aquamacs-styles-mode t nil (color-theme))
+ '(aquamacs-customization-version-id 190 t)
  '(default-frame-alist (quote ((tool-bar-lines . 0) (vertical-scroll-bars . right) (cursor-type . box) (background-toolbar-color . "#cccccccccccc") (bottom-toolbar-shadow-color . "#7a7a7a7a7a7a") (top-toolbar-shadow-color . "#f5f5f5f5f5f5") (color-theme-name . color-theme-arjen) (modeline . t) (fringe) (background-mode . dark) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (border-color . "black") (cursor-color . "yellow") (mouse-color . "sienna1") (background-color . "black") (foreground-color . "White") (font . "-apple-monaco-medium-r-normal--12-0-72-72-m-0-iso10646-1"))))
+ '(longlines-wrap-follows-window-size t)
  '(mac-command-modifier (quote alt))
  '(mac-font-panel-mode nil)
  '(nxml-slash-auto-complete-flag t)
@@ -23,7 +23,10 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(markdown-mode-default ((t (:inherit text-mode-default :slant normal :weight normal :height 130 :family "monaco"))) t))
+ '(autoface-default ((t (:inherit default :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :family "apple-monaco"))))
+ '(emacs-lisp-mode-default ((t (:inherit autoface-default))) t)
+ '(markdown-mode-default ((t (:inherit text-mode-default :slant normal :weight normal :height 130 :family "bitstream vera sans mono"))) t)
+ '(nxml-mode-default ((t (:inherit autoface-default :slant normal :weight normal :height 120 :family "bitstream vera sans mono"))) t))
 
 ;; for compatibility with older Aquamacs versions
  (defvar aquamacs-140-custom-file-upgraded t)
--- init.el
+++ init.el
@@ -1,22 +1,12 @@
 (setq load-path (cons "~/.emacs.d" load-path))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; language modes
+;; language modes  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;
-;; scala
+;; scala ;;;;;;;;;;;;;;;;
 ;;(add-to-list 'load-path "~/work/emacs/scala")  
 ;;(require 'scala-mode-auto)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; customizations
-
-;;;;;;;;;;;;;;;;
-;; color theme
-(add-to-list 'load-path "~/.emacs.d/color-theme")
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-arjen)
+;; customizations  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; set up alt key to work as META on Mac
 (set-keyboard-coding-system 'mac-roman)
@@ -26,83 +16,84 @@
 
 (put 'downcase-region 'disabled nil)
 
-;;;;;;;;;;;;;;;;
 ;; always use spaces instead of tabs
 (setq-default indent-tabs-mode nil)
 
+;; color theme
+(add-to-list 'load-path "~/.emacs.d/color-theme")
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-arjen)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; load some handy extensions
+;; handy extensions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; winring - for save/restore window configurations
 ;(require 'winring)
 ;(winring-initialize)
 
-;;;;;;;;;;;;;;;;
-;; ack
+;; ack 
 (require 'ack)    
 
-;;;;;;;;;;;;;;;;
 ;; docbook
 (autoload 'docbook-xml-mode "docbook-xml-mode" "Major mode for Docbook" t)
 
-;;;;;;;;;;;;;;;;
 ;; flyspell
-;; turn on flyspell mode
 (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
 (setq-default flyspell-mode t)
 ;; auto-load for flyspell mode
 (dolist (hook '(markdown-mode-hook))
   (add-hook hook (lambda () (flyspell-mode 1))))
 
-;;;;;;;;;;;;;;;;
 ;; ido
 (require 'ido)
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 
-;;;;;;;;;;;;;;;;
 ;; light symbol
 (autoload 'light-symbol "light-symbol" "Float-over highlighting for symbols." t)
 
-;;;;;;;;;;;;;;;;
 ;; magit (git support)
 (require 'magit)
 
-;;;;;;;;;;;;;;;;
+;; snippets ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; load up modes for msf-abbrev
 ;(require 'ruby-mode)
 
-;;;;;;;;;;;;;;;;
 ;; load up msf-abbrevs
-(add-to-list 'load-path "~/.emacs.d/msf-abbrev")
+;(add-to-list 'load-path "~/.emacs.d/msf-abbrev")
 ;; ensure abbrev mode is always on
-(setq-default abbrev-mode t)
+;(setq-default abbrev-mode t)
 ;; do not bug me about saving my abbreviations
-(setq save-abbrevs nil)
+;(setq save-abbrevs nil)
 
-;;;;;;;;;;;;;;;;
+;; msf-abbrev
+;(require 'msf-abbrev)
+;(setq msf-abbrev-verbose t) ;; optional
+;(setq msf-abbrev-root "~/.emacs.d/msf-abbrev/abbrevs")
+;(global-set-key (kbd "C-c l") 'msf-abbrev-goto-root)
+;(global-set-key (kbd "C-c a") 'msf-abbrev-define-new-abbrev-this-mode)
+;(msf-abbrev-load)
+
+
 ;; yasnippet
-;(add-to-list 'load-path
-;             "~/.emacs.d/plugins/yasnippet")
-;(require 'yasnippet) ;; not yasnippet-bundle
-;(yas/initialize)
-;(yas/load-directory "~/.emacs.d/plugins/yasnippet/snippets")
+(add-to-list 'load-path
+             "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/plugins/yasnippet/snippets")
 
-;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; snippets ;;
+
 ;; paraedit
-(autoload 'paredit-mode "paredit" "Minor mode for pseu  do-structurally editing Lisp code." t)
+;(autoload 'paredit-mode "paredit" "Minor mode for pseu  do-structurally editing Lisp code." t)
 
-;;;;;;;;;;;;;;;;
 ;; word count minor mode
 ;(setq load-path (cons (expand-file-name "~/.emacs.d") load-path))
 (autoload 'word-count-mode "word-count"
           "Minor mode to count words." t nil)
 (global-set-key "\M-+" 'word-count-mode)
 
-
-;;;;;;;;;;;;;;;;
 ;; wrap-region
 (add-to-list 'load-path "~/.emacs.d/wrap-region.el")
 (require 'wrap-region)
@@ -115,8 +106,7 @@
 ))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; associations
+;; associations ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; add markdown mode automatically
 (autoload 'markdown-mode "markdown-mode.el"
@@ -124,48 +114,26 @@
 (setq auto-mode-alist
    (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
-;;;;;;;;;;;;;;;;
 ;; html mode for HTML fils
-
 (setq auto-mode-alist (cons '("\\.html?$" . html-mode) auto-mode-alist))
 
-;;;;;;;;;;;;;;;;
 ;; Rake files are ruby too
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; load-em up
+;; load-em up ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'shell)
 (require 'ruby-mode)
 
-;;;;;;;;;;;;;;;;
-;; msf-abbrev
-(require 'msf-abbrev)
-(setq msf-abbrev-verbose t) ;; optional
-(setq msf-abbrev-root "~/.emacs.d/msf-abbrev/abbrevs")
-(global-set-key (kbd "C-c l") 'msf-abbrev-goto-root)
-(global-set-key (kbd "C-c a") 'msf-abbrev-define-new-abbrev-this-mode)
-(msf-abbrev-load)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; little found functions
+;; little found functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; found world count function
 (defun word-count nil "Count words in buffer" (interactive)
   (shell-command-on-region (point-min) (point-max) "wc -w"))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; slime
-
-;; (add-hook 'slime-connected-hook 'slime-ensure-typeout-frame)
-;; (require 'slime)
-;; (push (list 'clisp-2.35 (list "~/bin/lispbox-0.7/clisp-2.35/bin/clisp" "-ansi" "-K" "full" "-B" "/Applications/Lispbox/clisp-2.35/lib/clisp")) slime-lisp-implementations)
-;; (slime-setup)
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
